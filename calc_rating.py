4ef12d061db5845805100b1e1d42f8262cb7dc3c
Joao Paulo Targino <joaotargino@gmail.com>
2017-12-19 12:40:06 -0300

    update: highlight project name update: calculating total of projects and percentage of feedback

--- calc_rating.py
+++ calc_rating.py
@@ -2,10 +2,12 @@
 
 import requests
 import sys
+
+
 # import json
 
 
-def rating_by_project(project_name):
+def rating_by_project(project_name, d_submissions):
     for student_feedback in feedback_history:
         dict_ratings[student_feedback["rating"]] += 1
         if student_feedback["project"]["name"] == project_name:
@@ -14,17 +16,38 @@ def rating_by_project(project_name):
     grade_project = dict_project[5] * 5 + dict_project[4] * 4 + dict_project[
         3] * 3 + dict_project[2] * 2 + dict_project[1]
     total_feedback_by_project = sum(dict_project.values())
+    total_reviews = d_submissions.get(project_name)
     if total_feedback_by_project > 0:
         grade_project = float(grade_project) / sum(dict_project.values())
         # print dict_project
-        print "    - %s is: %s in a total of %d ratings" % (
-            project_name, get_color(round(grade_project, 2)),
-            total_feedback_by_project)
+        print "    - %s is: %s in a total of %d ratings out of %s reviews " \
+              "(%s%% feedback)" % (
+                  get_color(project_name), get_color(round(grade_project, 2)),
+                  total_feedback_by_project, total_reviews,
+                  calc_percentage(total_feedback_by_project, total_reviews))
+
+
+def get_total_reviews(submissions):
+    d_submissions = {}
+    for submission in submissions:
+        if submission["result"] == "passed" or \
+                submission["result"] == "failed":
+            if submission["project"]["name"] in d_submissions.keys():
+                d_submissions[submission["project"]["name"]] += 1
+            else:
+                d_submissions[submission["project"]["name"]] = 0
+    return d_submissions
+
+
+def calc_percentage(feedback_project, total_reviews):
+    return str(feedback_project * 100 / total_reviews)
 
 
 def get_color(current_grade):
     black, red, green, yellow, blue, magenta, cyan, white = range(8)
-    if current_grade <= 3.0:
+    if isinstance(current_grade, basestring):
+        return "\x1b[1;%dm" % (30 + cyan) + "%s" % current_grade + "\x1b[0m"
+    elif current_grade <= 3.0:
         color = red
     elif grade <= 4.20:
         color = blue
@@ -56,12 +79,11 @@ if q.upper() != "N" and q != "":
     start_date = raw_input('Enter the start date (yyyy-mm-dd):')
     end_date = raw_input('Enter the end date (yyyy-mm-dd):')
 
-
 STUDENTS_FEEDBACK_URL_ALL = \
     'https://review-api.udacity.com/api/v1/me/' \
     'student_feedbacks?start_date=%s&end_date=%s' % (start_date, end_date)
 
-print "Calculating..."
+print "Calculating... This may take a while... So, what's up?"
 # print "\n=================================="
 feedback_history = requests.get(STUDENTS_FEEDBACK_URL_ALL, headers=headers)
 
@@ -79,12 +101,24 @@ certification = certification.json()
 
 # print "=================================="
 
+SUBMISSIONS_COMPLETED_URL = 'https://review-api.udacity.com/api/' \
+                            'v1/me/submissions/completed'
+submissions_completed = requests.get(SUBMISSIONS_COMPLETED_URL,
+                                     headers=headers)
+submissions_completed = submissions_completed.json()
+
+dict_submissions = get_total_reviews(submissions_completed)
+
 dict_ratings = {5: 0, 4: 0, 3: 0, 2: 0, 1: 0}
 
 for feedback in feedback_history:
     dict_ratings[feedback["rating"]] += 1
 
-print "Number of ratings: %d" % len(feedback_history)
+total_reviews = sum(dict_submissions.values())
+print "Number of ratings: %d out of %s reviews " \
+      "(%s%% feedback) " % (len(feedback_history), total_reviews,
+                            calc_percentage(
+                                len(feedback_history), total_reviews))
 print dict_ratings
 
 grade = dict_ratings[5] * 5 + dict_ratings[4] * 4 + dict_ratings[3] * 3 \
@@ -103,4 +137,4 @@ for project in certification:
 print "Your grade as a Project Reviewer of:"
 for project in project_list:
     dict_project = {5: 0, 4: 0, 3: 0, 2: 0, 1: 0}
-    rating_by_project(project)
+    rating_by_project(project, dict_submissions)
